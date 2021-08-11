fa640d871945cdb03dd15c684aa5576b7848c9f6
unknown <vinaymurthy95@gmail.com>
2021-08-11 21:02:13 +0530

    adding java file

---
+++ Exp1.java
@@ -0,0 +1,16 @@
+package agiliz;
+
+public class Exp1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=2,y=3;
+		System.out.println(3*x+2*y);
+				
+				
+				
+				//i)	x=2; y=3; 3*x+2*y;
+
+	}
+
+}
